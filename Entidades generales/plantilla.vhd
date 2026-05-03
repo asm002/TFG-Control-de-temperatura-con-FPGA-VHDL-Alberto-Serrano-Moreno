d93825aafe440c13d708d1a07e3406b503582040
asm002 <asm00152@red.ujaen.es>
2026-05-03 17:48:06 +0200

    Create plantilla.vhd

---
+++ Entidades generales/plantilla.vhd
@@ -0,0 +1,32 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity PLANTILLA is
+	PORT(
+			
+	);
+END entity;
+
+architecture Behavioral of PLANTILLA is
+	-- < definicion de señales internas, tipos y constantes > --
+	
+	
+	begin
+		-- < mapeo de entidades internas > --
+		
+		
+		-- < logica combinacional; asignaciones directas > --
+		
+		
+		-- < logica secuencial, procesos > --
+		process(CLK)
+			-- < definicion de variables, tipos y constantes >
+			
+			begin			
+				if rising_edge(CLK) then
+				
+				end if;
+		end process;
+		
+end architecture;
