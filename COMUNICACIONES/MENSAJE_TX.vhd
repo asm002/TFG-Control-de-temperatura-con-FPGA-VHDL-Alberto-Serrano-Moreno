35fcfa9ae3c2d9f394c95e87b4640fefb230c076
asm002 <asm00152@red.ujaen.es>
2026-06-14 20:59:02 +0200

    COMS: envío de mensaje completo
    - Ahora el mensaje que se envía es de 28 bytes y cumple el formato que requiere la app de Windows.
    - En consecuencia, dicha app ya muestra y grafica los datos en tiempo real correctamente, adaptado a los rangos designados
    - El envío de datos queda completo por tanto, excepto que por ahora se mandan valores constantes de consigna, error y pwm.
    - Cuando el modulo de PID esté completo y existan esos registros, se deberá modificar nuevamente MENSAJE_TX para enviar los valores en tiempo real en lugar de estas constantes de prueba

--- COMUNICACIONES/MENSAJE_TX.vhd
+++ COMUNICACIONES/MENSAJE_TX.vhd
@@ -5,9 +5,12 @@ use IEEE.numeric_std.all;
 library work;
 use work.CONFIG_PROYECTO.all;
 
+--  Formato completo de mensajes:
+--  DATA <CONSIGNA.0> <TEMPERATURA.0> <ERROR.0> <PWM>
+
 entity MENSAJE_TX is
     port(
-        indice : in integer range 0 to 10;
+        indice : in integer range 0 to MSG_N_BYTES-1;
         byte_out : out std_logic_vector(7 downto 0);
         
         -- TEMPERATURA (aprovechamos los convertidores BCD de la adquisicion)
@@ -41,13 +44,38 @@ architecture Behavioral of MENSAJE_TX is
             x"41" when 3,  -- 'A'
             x"20" when 4,  -- Espacio ' '
             
-            -- "-99.9/n"
-            ascii_signo_temp when 5, -- signo de la temperatura
-            x"3" & bus_temperatura.bcd_decenas when 6,
-            x"3" & bus_temperatura.bcd_unidades when 7,
-            x"2E" when 8, -- punto (decimal)
-            x"3" & bus_temperatura.bcd_decimas when 9,
-            x"0A" when 10;  -- salto de linea (LF)
+            -- <CONSIGNA.0> <+99.9 >
+            x"2D" when 5,   -- -
+            x"30" when 6,   -- 0
+            x"35" when 7,   -- 5
+            x"2E" when 8,   -- punto
+            x"37" when 9,   -- 7
+            x"20" when 10,  -- espacio
+            
+            -- <TEMPERATURA.0> <+99.9 >
+            ascii_signo_temp                        when 11,  -- signo de la temperatura
+            x"3" & bus_temperatura.bcd_decenas      when 12,
+            x"3" & bus_temperatura.bcd_unidades     when 13,
+            x"2E"                                   when 14,  -- punto (decimal)
+            x"3" & bus_temperatura.bcd_decimas      when 15,
+            x"20"                                   when 16,  -- espacio
+            
+            -- <ERROR.0> <+199.8 >
+            x"2B" when 17,   -- +
+            x"31" when 18,   -- 1
+            x"39" when 19,   -- 9
+            x"39" when 20,   -- 9
+            x"2E" when 21,   -- punto
+            x"38" when 22,   -- 8
+            x"20" when 23,  -- espacio
+            
+            -- <PWM> <255\n>
+            x"32" when 24,  -- 2
+            x"35" when 25,  -- 5
+            x"35" when 26,  -- 5
+            x"0A" when 27,  -- salto de linea (LF)
+            
+            x"21" when others;  -- signo de exclamacion
         
         
         
